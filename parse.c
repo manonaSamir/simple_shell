f6238735b28fb2554b24ce0a16750ef48b0fe94e
manonaSamir <bena.yalla@yahoo.com>
2023-11-17 00:49:11 +0200

    simple sheel fixes

--- parse.c
+++ parse.c
@@ -10,10 +10,12 @@
 char **split_command(char *command)
 {
 	int buffer = 64, i = 0;
-	char *path = NULL, *test = NULL,
-		 **Line_size = NULL, *token = NULL;
+	char **Line_size = NULL,
+		 *small_str = NULL, *token = NULL,
+		 *path = NULL, *test = NULL;
 
-	if (removeSpaces(command) == NULL)
+	test = removeSpaces(command);
+	if (test == NULL)
 		return (0);
 
 	Line_size = malloc(buffer * sizeof(char *));
@@ -21,20 +23,36 @@ char **split_command(char *command)
 	path = getenv("PATH");
 
 	token = strtok(command, " \t\r\n\a\"");
+
 	if (access(token, F_OK) == -1 && path == NULL)
 	{
 		fprintf(stderr, "%s: 1: %s: not found\n", commands, command);
 		exit(127);
 	}
+
 	while (token != NULL)
 	{
 		if (token[0] == '#')
 			break;
 		if (!_strstr(token, "/bin/") && !_strstr(token, "/")
-		&& concate_command(token) == 1)
-			Line_size[i++] = addingstr(token);
+		 && concate_command(token) == 1)
+		{
+			small_str = malloc((strlen("/bin/") + strlen(token) + 1) * sizeof(char));
+			if (!small_str)
+			{
+				fprintf(stderr, "allocation error in split_line: tokens\n");
+				exit(EXIT_FAILURE);
+				free_memory(Line_size, i);
+			}
+			_strcpy(small_str, "/bin/");
+			_strcat(small_str, token);
+			Line_size[i++] = small_str;
+			small_str = NULL;
+			free(small_str);
+		}
 		else
 			Line_size[i++] = token;
+
 		if (i >= buffer)
 		{
 			buffer += buffer;
